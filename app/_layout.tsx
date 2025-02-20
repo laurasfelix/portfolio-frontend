302002e4a292ddfd8184784bc5b92f26e9b3fc54
laurasfelix <laurasfelix@hotmail.com.br>
2025-02-19 17:58:45 -0800

    boot works

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import {View} from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    'SF-Pro-Rounded-Regular': require('../assets/fonts/SF-Pro-Rounded-Regular.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        LOADING
+      </View>
+    );
+  }
+  return <Stack screenOptions={{
+    headerShown: false,
+  }}/>;
+
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,61 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Dimensions} from "react-native";
+import { VideoView, useVideoPlayer} from 'expo-video';
+import { useEffect } from "react";
+import Animated, { FadeIn, ReduceMotion } from 'react-native-reanimated';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
 
 export default function Index() {
+
+
+  const videoSource = require("../assets/videos/boot.mp4");
+
+  const player = useVideoPlayer(videoSource, player => {
+    player.loop = true;
+    player.volume = 0;
+  
+  });
+
+  useEffect(() => {
+    if (player) {
+      console.log("Playing video...");
+      player.play();
+    }
+  }, [player]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>laura's profile</Text>
+    <View style={styles.container}>
+    
+      <Animated.View
+        
+        entering={FadeIn.duration(8000).reduceMotion(ReduceMotion.Never)}
+        style={styles.main}
+      >
+          <VideoView style={styles.video} player={player} allowsFullscreen nativeControls={false} allowsPictureInPicture contentFit="cover" /> 
+          {/* <Text>laura's profile</Text> */}
+        
+      
+      </Animated.View>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+    video:{
+      width: "100%",
+      height:"100%",
+    
+    },
+    main:
+    {
+   
+     width: screenWidth,
+     height: screenHeight,
+    }, 
+    container:{
+      backgroundColor: "black",
+    }
+});
