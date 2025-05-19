c2d483b15b0ec995570cb16ca291de11f0df5ec5
laurasfelix <laurasfelix@hotmail.com.br>
2025-05-19 17:58:28 -0500

    logo + image fix?

--- utils/itemInfo.tsx
+++ utils/itemInfo.tsx
@@ -12,68 +12,68 @@ const itemInfo: ItemInfoProp = [
     [{
         "title":"introduction",
         "text":["aaa"],
-        "icon": require("@/public/images/user_icon.svg"),
+        "icon": "/images/user_icon.svg",
     },
     {
         "title":"education",
         "text":["aaa"],
-        "icon": require("@/public/images/education.svg"),
+        "icon": "/images/education.svg",
     },
     {
         "title":"skills",
         "text":["aaa"],
-        "icon": require("@/public/images/idea.svg"),
+        "icon": "/images/idea.svg",
     },
    ],
   
     [{
         "title":"swe intern @ duolingo",
         "text":[""],
-        "icon": require("@/public/images/duolingo 1.svg"),
+        "icon": "/images/duolingo 1.svg",
     },
     {
         "title":"ta @ northwestern cs",
         "text":[""],
-        "icon": require("@/public/images/northwestern 1.svg"),
+        "icon": "/images/northwestern 1.svg",
     },
     {
         "title":"scholar @ citadel",
         "text":[""],
-        "icon": require("@/public/images/citadel.svg"),
+        "icon": "/images/citadel.svg",
     }],
 
 
     [{
         "title":"bread",
         "text":[""],
-        "icon": require("@/public/images/bread.svg"),
+        "icon": "/images/bread.svg",
     },
     {
         "title":"sunflower",
         "text":[""],
-        "icon": require("@/public/images/sunflower.svg"),
+        "icon": "/images/sunflower.svg",
     },
     {
         "title":"pupally",
         "text":[""],
-        "icon": require("@/public/images/puppy.svg"),
+        "icon": "/images/puppy.svg",
     }],
 
     [{
         "title":"folder",
         "text":[""],
-        "icon": require("@/public/images/folder.svg"),
+        "icon": "/images/folder.svg",
     },
     {
         "title":"folder",
         "text":[""],
-        "icon": require("@/public/images/folder.svg"),
+        "icon": "/images/folder.svg",
     }],
  
     [{
         "title":"email",
         "text":[""],
-        "icon": require("@/public/images/email.svg"),
+        "icon": "/images/email.svg",
     }],
 
 
