52778266cbab35427b15ca381066ed1158089a02
laurasfelix <laurasfelix@hotmail.com.br>
2025-05-31 16:24:39 -0500

    added and populated colors.ts

---
+++ utils/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  green: "#7CBC29",
+  red: "#FF0505",
+  yellow: "#FFAB03",
+  shadowGreen: "#678243",
+  shadowRed: "#CC2F2F",
+  shadowYellow:"#CC8802",
+  darkGreen: "#65A512",
+  darkRed: "#D70000",
+  darkYellow:"#CB8700",
+  lightGreen:"#BEE689",
+  lightRed: "#FFC1C1",
+  lightYellow: "#FFD98E",
+  textGreen:"#3F6013",
+  textRed:"#9B0000",
+  textYellow:"#B1842B"
+
+};
